e96de3507dde2b99adb39850f84825fa2a80c5aa
Kirillp1988 <kirillp1988@gmail.com>
2022-04-27 16:36:30 +0300

    Kirill update

---
+++ backend/src/main/java/com/example/digitom/domain/role/RoleController.java
@@ -0,0 +1,9 @@
+package com.example.digitom.domain.role;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/roll")
+public class RoleController {
+}
