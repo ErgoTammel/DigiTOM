7446241b8b2a627c8a72440f1417806ff1994eee
Kirillp1988 <kirillp1988@gmail.com>
2022-04-27 13:52:38 +0300

    Kirill update

---
+++ backend/db/digitom_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO digi_tom.role (name) VALUES ('Altöövõtja');
+INSERT INTO digi_tom.role (name) VALUES ('Peatöövõtja');
+
+INSERT INTO digi_tom.user (email, password) VALUES ('kirillp1988@gmail.com', '123');
+INSERT INTO digi_tom.user (email, password) VALUES ('ergotammel@gmail.com', '456');
+INSERT INTO digi_tom.user (email, password) VALUES ('silversomma@gmail.com', '789');
+
