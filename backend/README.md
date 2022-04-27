eeda857235770f8b9153c31bd74c63b0516f4043
ErgoTammel <ergotammel@gmail.com>
2022-04-27 11:38:05 +0300

    First commit.

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+"# DigiTOM" 
+"# DigiTOM" 
+"# DigiTOM" 
---
+++ backend/src/test/java/com/example/digitom/DigitomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
