11dc2caeb3148a8d35ef46372aa6a7908941f76f
Kirillp1988 <kirillp1988@gmail.com>
2022-05-05 14:18:40 +0300

    Validation

--- backend/src/main/java/com/example/digitom/service/image/ImageController.java
+++ backend/src/main/java/com/example/digitom/service/image/ImageController.java
@@ -1,15 +1,20 @@
 package com.example.digitom.service.image;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/image")
 public class ImageController {
 
+    @Resource
     private ImageService imageService;
 
+    @PostMapping("/add")
     public void addReportPicture(@RequestBody ReportPictureRequest reportPictureRequest) {
         imageService.addNewReportPicture(reportPictureRequest);
     }
