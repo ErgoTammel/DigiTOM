e3420f6371103144bf92ca77fba8c59df9a3e71d
Kirillp1988 <kirillp1988@gmail.com>
2022-05-06 15:50:10 +0300

    Validation

--- backend/src/main/java/com/example/digitom/domain/company/CompanyService.java
+++ backend/src/main/java/com/example/digitom/domain/company/CompanyService.java
@@ -4,7 +4,6 @@ import com.example.digitom.service.account.RegistrationRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
---
+++ backend/src/main/java/com/example/digitom/service/inspection/ReportResultResponse.java
@@ -0,0 +1,11 @@
+package com.example.digitom.service.inspection;
+
+import lombok.Data;
+
+@Data
+public class ReportResultResponse {
+    private Integer safeSum;
+    private Integer notSafeSum;
+    private Double tom;
+
+}
